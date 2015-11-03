72ba6782e056ebd4856864b23ee060a5621ccd9d
Fondant13200 <guillaume.perez@imerir.com>
2015-11-04 00:12:20 +0100

    Start Move
    Mise en place du premier programme se connectant au Kuka et faisant
    déplacer celui-ci avec la souris 3D

---
+++ Class_Kuka_Manager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using NLX.Robot.Kuka.Controller;
+
+namespace Projet_Kuka
+{
+    class Class_Kuka_Manager
+    {
+        RobotController robot = new RobotController();
+
+        // Constructeur permettant de ce connecter au robot
+        public Class_Kuka_Manager()
+        {
+            try
+            {
+                robot.Connect("192.168.1.1");
+            }
+            catch (IOExeception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        // Fonction pour Start la connexion avec le Kuka
+        public void Start()
+        {
+            robot.StartRelativeMovement();
+        }
+
+        // Fonction pour Stopper la connexion avec le Kuka
+        public void Stop()
+        {
+            robot.StopRelativeMovement();
+        }
+
+
+        // Fonction permettant d'utiliser les translations et rotations de la souris et de les envoyer au Kuka
+        public void Kuka_Move(TDx.TDxInput.IVector3D translation, TDx.TDxInput.AngleAxis rotation)
+        {
+           // robot.StartRelativeMovement();
+            // List<CartesianPosition> position = new List<CartesianPosition>();
+            /*
+            double x, y, z;
+            
+           
+
+
+                x = robot.GetCurrentPosition().X;
+                y = robot.GetCurrentPosition().Y;
+                z = robot.GetCurrentPosition().Z;
+
+                Console.WriteLine("position x : " + x + ", position y : " + y + ", position z : " + z);
+            */
+            Console.WriteLine(" je bouge");
+
+            // on recupere les valeurs des translations et rotations de la souris 3D
+            var Position =(new CartesianPosition
+                {
+                    X = translation.X,
+                    Y = translation.Y,
+                    Z = translation.Z,
+                    A = rotation.X,
+                    B = rotation.Y,
+                    C = rotation.Z,
+                });
+            Console.WriteLine(Position.X + " ; " + Position.Y + " ; " + Position.Z);
+
+            // on envoi ces valeurs au Kuka
+            robot.SetRelativeMovement(Position);
+
+           // robot.StopRelativeMovement();
+        }
+    }
+
+    [Serializable]
+    internal class IOExeception : Exception
+    {
+        public IOExeception()
+        {
+        }
+
+        public IOExeception(string message) : base(message)
+        {
+        }
+
+        public IOExeception(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IOExeception(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Class_Mouse.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLX.Robot.Kuka.Controller;
+
+namespace Projet_Kuka
+{
+    class Class_Mouse
+    {
+        static TDx.TDxInput.Device device;
+        static Class_Kuka_Manager my_kuka;
+
+        // Constructeur de la Class_Mouse où on récupère l'instance de la Class_Kuka_Manager que l'on va utiliser
+        public Class_Mouse(Class_Kuka_Manager kuka)
+        {
+            my_kuka = kuka;
+        }
+
+
+        // Fonction permettant de récupérer les données de mouvement de la souris
+        public void Data_Mouse()
+        {
+            Console.WriteLine("Programm Start ....");
+
+            device = new TDx.TDxInput.Device();
+            device.Connect();
+            bool test = true;
+
+            my_kuka.Start();
+
+            while (test)
+            {
+                var translation = device.Sensor.Translation;
+                var rotation = device.Sensor.Rotation;
+
+                // diviser translation pour normaliser les valeurs à envoyer au Kuka
+                // ces valeurs sont déterminer manuellement par l'appel du min et du max.
+                translation.X = translation.X / 2729;
+                translation.Y = translation.Y / 2832;
+                translation.Z = translation.Z / 2815;
+
+                Console.WriteLine("Translation : " + translation.X + ";" + translation.Y + ";" + translation.Z);
+                Console.WriteLine("Rotation : " + rotation.X + ";" + rotation.Y + ";" + rotation.Z);
+
+                // Mettre en place un lissage permettant de faire déplacer le Kuka de manière plus fluide en supprimant les "bruits" générer par la souris
+
+
+
+
+                // On appelle la fonction Move de la Class_Kuka_Manager
+                my_kuka.Kuka_Move(translation, rotation);
+                Console.WriteLine(" j'ai bougé");
+
+                System.Threading.Thread.Sleep(50);
+            }
+
+            Console.WriteLine("Programm End, press a key to exit....");
+            Console.ReadKey();
+            my_kuka.Stop();
+        }
+    }
+}
+
+
